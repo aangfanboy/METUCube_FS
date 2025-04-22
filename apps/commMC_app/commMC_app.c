b8bcade9e6debdffcee5959616f590feb37da1c8
burak laptop <buraktoy161@gmail.com>
2025-04-22 17:14:33 +0300

    comma fix

--- apps/commMC_app/commMC_app.c
+++ apps/commMC_app/commMC_app.c
@@ -24,7 +24,7 @@ void COMMMC_App_Main(void){
 
         if (status == CFE_SUCCESS){
             COMMMC_APP_TaskPipe(SBBufPtr);
-            COMMMC_APP_SEND_DATA_TO_IP()
+            COMMMC_APP_SEND_DATA_TO_IP();
 
         }else{
             CFE_EVS_SendEvent(COMMMC_APP_PIPE_ERR_EID, CFE_EVS_EventType_ERROR, "COMMMC App: SB Pipe Read Error, RC = 0x%08X", status);
