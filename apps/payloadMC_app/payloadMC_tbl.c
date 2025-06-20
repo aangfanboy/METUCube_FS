3a2ae9527f9a82de0e2cd6c608a75f2a9cbf53a3
burak laptop <buraktoy161@gmail.com>
2025-06-20 14:42:25 +0300

    table tes

--- apps/payloadMC_app/payloadMC_tbl.c
+++ apps/payloadMC_app/payloadMC_tbl.c
@@ -1,6 +1,17 @@
 
 #include "cfe_tbl_filedef.h" /* Required to obtain the CFE_TBL_FILEDEF macro definition */
 
+#include "common_types.h"
+
+/*
+** Example Table structure
+*/
+typedef struct
+{
+    uint16 Int1;
+    uint16 Int2;
+} PAYLOADMC_ExampleTable_t;
+
 PAYLOADMC_ExampleTable_t ExampleTable = {15, 22};
 
 CFE_TBL_FILEDEF(ExampleTable, PAYLOADMC.ExampleTable, Table Utility Payload Table, payloadMC_tbl.tbl)
