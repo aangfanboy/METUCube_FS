f6a4758294ff9cadecb60be5057a74b14508f8ba
burak laptop <buraktoy161@gmail.com>
2025-06-23 15:41:12 +0300

    payload standarts implementation

--- apps/payloadMC_app/fsw/src/payloadMC_app_dispatch.c
+++ apps/payloadMC_app/fsw/src/payloadMC_app_dispatch.c
@@ -5,7 +5,6 @@
 void PAYLOADMC_appTaskPipe(const CFE_SB_Buffer_t *SBBufPtr)
 {
     CFE_SB_MsgId_t MsgId    = CFE_SB_INVALID_MSG_ID;
-    CFE_Status_t   status   = 0;
 
     CFE_MSG_GetMsgId(&SBBufPtr->Msg, &MsgId);
 
@@ -21,12 +20,7 @@ void PAYLOADMC_appTaskPipe(const CFE_SB_Buffer_t *SBBufPtr)
             CFE_EVS_SendEvent(PAYLOADMC_MSG_RECEIVED_EID, CFE_EVS_EventType_INFORMATION,
                               "PAYLOADMC: Received HK request");
 
-            status = PAYLOADMC_APP_SEND_HK_TO_SB();
-            if (status != CFE_SUCCESS)
-            {
-                CFE_EVS_SendEvent(PAYLOADMC_APP_HK_SEND_ERR_EID, CFE_EVS_EventType_ERROR,
-                                  "PAYLOADMC: Error sending HK data to SB");
-            }
+            PAYLOADMC_APP_SEND_HK_TO_SB();
             break;
 
         default:
