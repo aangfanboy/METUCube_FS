ad21b89420a71e02d22c43883edf2f81f2004f76
burak laptop <buraktoy161@gmail.com>
2025-06-28 20:00:46 +0300

    payload standarts implementation

--- apps/payloadMC_app/fsw/tables/payloadMC_app_config_tbl.c
+++ apps/payloadMC_app/fsw/tables/payloadMC_app_config_tbl.c
@@ -17,5 +17,5 @@
     250  // someRandomTemperatureConfig (example value, can be adjusted)
 };
 
-CFE_TBL_FILEDEF(PAYLOADMC_ConfigTbl, PAYLOADMC_APP.ConfigTable, PayloadCT, payloadMCConf.tbl)
+CFE_TBL_FILEDEF(PAYLOADMC_ConfigTbl, PAYLOADMC_APP.ConfigTable, PayloadCT, payloadMC_Config.tbl)
 
