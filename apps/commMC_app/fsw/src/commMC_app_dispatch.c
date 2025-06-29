14deb8acfd2661d6313622a83af6c89583c79ef2
burak laptop <buraktoy161@gmail.com>
2025-06-29 15:39:36 +0300

    comm message integration

--- apps/commMC_app/fsw/src/commMC_app_dispatch.c
+++ apps/commMC_app/fsw/src/commMC_app_dispatch.c
@@ -24,7 +24,7 @@ void COMMMC_appTaskPipe(const CFE_SB_Buffer_t *SBBufPtr)
                     break;
                 default:
                     CFE_EVS_SendEvent(COMMMC_INVALID_CMD_ERR_EID, CFE_EVS_EventType_ERROR,
-                                        "COMMMC: Invalid command task ID = 0x%x", CmdPkt->CommandData[4]);
+                                        "COMMMC: Invalid command task ID = 0x%x", CmdPkt->CommandData[1]);
                     break;
             }
             break;
