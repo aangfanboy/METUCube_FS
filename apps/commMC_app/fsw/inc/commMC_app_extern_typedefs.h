98771ef702ee8edb54378a50a32df40b5eb9f296
burak laptop <buraktoy161@gmail.com>
2025-06-29 15:13:05 +0300

    comm message integration

--- apps/commMC_app/fsw/inc/commMC_app_extern_typedefs.h
+++ apps/commMC_app/fsw/inc/commMC_app_extern_typedefs.h
@@ -11,5 +11,16 @@
  #define COMMMC_EXTERN_TYPEDEFS_H
 
  #define COMMMC_APP_COMMAND_TASK_ID_SEND_MINIMAL_TM_TO_GROUND 0x0001
+ /**
+  * @brief Command Packet Structure for CommMC Application
+  * 
+  * This structure defines the command packet that the CommMC application uses to send commands.
+  * It includes a command task ID to specify the action to be taken.
+  * */
+
+    typedef struct 
+    {
+        uint16 CommandTaskId; /**< \brief Command task ID */
+    } COMMMC_APP_CommandPacket_t;
  
  #endif /* COMMMC_EXTERN_TYPEDEFS_H */
