778f483cd3763514a3802e73f2a268facde76872
burak laptop <buraktoy161@gmail.com>
2025-07-13 16:43:56 +0300

    comm message integration

--- apps/commMC_app/fsw/src/commMC_app_utils.h
+++ apps/commMC_app/fsw/src/commMC_app_utils.h
@@ -6,6 +6,6 @@
 #include "crc-32.h"
 
 CFE_Status_t compute_sha256(FILE *file, unsigned char fileHash[32]);
-CFE_Status_t compute_crc32(unsigned char *data, size_t length, uint32_t *crc32);
+CFE_Status_t compute_crc32(const void *data, size_t length, uint32_t *crc32);
 
 #endif /* COMMMC_APP_UTILS_H */
